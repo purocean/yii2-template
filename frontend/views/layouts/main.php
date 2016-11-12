0444c7b18a0110454b39662278f20008bd191bf4
Yang <purocean@gmail.com>
2016-11-13 00:34:22 +0800

    Add site name

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => Yii::$app->params['siteName'],
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',

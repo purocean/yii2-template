e268ece30d506c9fdfb1cfbcbb50bd5330638559
Yang <purocean@gmail.com>
2017-02-05 17:27:15 +0800

    修复测试配置

--- application/config/test.php
+++ application/config/test.php
@@ -1,4 +1,12 @@
 <?php
 return [
     'id' => 'application-tests',
+    'components' => [
+        'assetManager' => [
+            'basePath' => __DIR__ . '/../web/assets',
+        ],
+        'request' => [
+            'enableCsrfValidation' => false,
+        ],
+    ],
 ];
